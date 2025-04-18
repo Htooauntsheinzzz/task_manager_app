43547e4f843d1d45946edc399c978f5ff3dd0943
Htoo Aunt Shein <htooauntshein@Htoos-MacBook-Pro.local>
2025-04-18 21:50:44 +0700

    Second Commit

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "http://localhost:8800/api";
+// const API_URL = "http://localhost:8800/api";
+
+const API_URL = "https://task-manager-standlone.onrender.com"
 
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
