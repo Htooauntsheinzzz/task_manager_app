69e4dbfd3551662806e42781f8dcf71b208ea0bd
Htoo Aunt Shein <74733199+Htooauntsheinzzz@users.noreply.github.com>
2025-04-19 03:31:17 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","https://taskmanager-standlone.netlify.app"],
+    origin: ["http://localhost:3000", "http://localhost:3001","https://task-manager-app-rho-six.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
